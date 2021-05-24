4b769a02f9f6c4e9a13458d3e2f6f14a82118ea2
gunheui <pgh5986@gmail.com>
2021-05-24 22:01:48 +0900

    Add : restart bug fix

--- src/component/RestartButton.js
+++ src/component/RestartButton.js
@@ -4,7 +4,12 @@ import { Button } from "react-bootstrap";
 
 const RestartButton = ({ isGameOver }) => {
   return (
-    <Button href="/" variant="outline-primary" size="lg" type="reset">
+    <Button
+      href="https://gunheui.github.io/Avoid-Obstacle/"
+      variant="outline-primary"
+      size="lg"
+      type="reset"
+    >
       Restart
     </Button>
   );
